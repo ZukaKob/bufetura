2ed9524527ce7ee5aa8aff3772005d44922ee4b2
zuka <zukakokobakhidze@gmail.com>
2022-10-27 18:58:53 +0400

    make it better

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express') 
 const mongoose = require('mongoose') 
+const cors = require('cors')
 require('dotenv').config()
 
 const app = express() 
 
+app.use(cors('*'))
 app.use(express.json())
 app.use(express.urlencoded({extended:true})) 
 
