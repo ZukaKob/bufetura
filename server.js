32bea7639bfc380bea7510d1e7500f514c73b53a
zuka <zukakokobakhidze@gmail.com>
2022-10-27 18:49:57 +0400

    make it better

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use('/api/v1', require('./routes/auth_routes'))
 app.use(require('./routes/admin_routes')) 
 
 mongoose    
-    .connect(process.env.MONGO_URI)
+    .connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
         app.listen(process.env.PORT, () => {
             console.log(`Database set, server started on port: ${process.env.PORT}`)
